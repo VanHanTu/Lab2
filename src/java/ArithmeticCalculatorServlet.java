7bdd99607fefb5d53fabb8943be000ec658cbd43
759841 <759841@ELF8IT221610.ACDM.DS.SAIT.CA>
2019-09-16 13:29:59 -0600

    Lab 2

--- src/java/ArithmeticCalculatorServlet.java
+++ src/java/ArithmeticCalculatorServlet.java
@@ -26,7 +26,40 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
+        String f = request.getParameter("first");
+        String s = request.getParameter("second");
+        String calc = request.getParameter("calculation");
+        try
+        {
+            int fnum = Integer.parseInt(f);
+            int snum = Integer.parseInt(s);
+            int result ;
+            
+            if(calc.equals("+"))
+            {
+                result = fnum + snum;
+                request.setAttribute("result",result);
+            }
+            if(calc.equals("-"))
+            {
+                result = fnum - snum;
+                request.setAttribute("result",result);
+            }
+            if(calc.equals("*"))
+            {
+                result = fnum * snum;
+                request.setAttribute("result",result);
+            }
+            if(calc.equals("%"))
+            {
+                result = fnum / snum;
+                request.setAttribute("result",result);
+            }
+        }catch (NumberFormatException e)
+        {
+            request.setAttribute("result", "You have to put valid number");
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
     }
 
  
